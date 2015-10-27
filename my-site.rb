c9d03b21ab6309bd936a2b0cfe78614c47ee9aaa
Lauren Granger <lr.granger21@gmail.com>
2015-10-27 16:25:44 -0700

    wave 1 done

--- my-site.rb
+++ my-site.rb
@@ -8,6 +8,12 @@ class MySite < Sinatra::Base
   get "/blog" do
     erb :blog
   end
+  get "/blog/girls_cant_do_math" do
+    erb :"posts/girls_cant_do_math"
+  end
+  get "/blog/they_just_fall" do
+    erb :"posts/they_just_fall"
+  end
   get "/about_me" do
     erb :about_me
   end
