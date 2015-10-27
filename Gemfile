81e49a861728e8a7f683814b282d7756969f5c95
Lauren Granger <lr.granger21@gmail.com>
2015-10-27 14:22:17 -0700

    set up

---
+++ Gemfile
@@ -0,0 +1,9 @@
+# A sample Gemfile
+source "https://rubygems.org"
+ruby "2.1.2"
+
+gem "sinatra"
+
+group :development do
+  gem "pry"
+end
--- my-site.rb
+++ my-site.rb
@@ -2,7 +2,16 @@ require "sinatra"
 
 class MySite < Sinatra::Base
 
-  get "/" do
-
+  get "/index" do
+    erb :index
+  end
+  get "/blog" do
+    erb :blog
+  end
+  get "/about_me" do
+    erb :about_me
+  end
+  get "/projects" do
+    erb :projects
   end
 end
